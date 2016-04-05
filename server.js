1ab627d31f8d6ff1aa547054b502f49896293623
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-05 10:52:42 -0400

    initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
+		next();
+	}
+}
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(port, function(){
+	console.log('Express Server Started on port ' + port);
+});
