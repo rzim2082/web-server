869d45b35970da4657993cc7fb01e72b5b41da8b
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-05 12:17:53 -0400

    added middleware.js

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var middleware = require('./middleware.js');
 
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next){
-		
-		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
-		next();
-	}
-}
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
